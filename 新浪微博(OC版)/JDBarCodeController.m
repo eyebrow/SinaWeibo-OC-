f98c6d2bcc5971c54049c4f615cf48421852748d
JiangDi <JiangDi@JiangDi.local>
2016-05-09 13:21:00 +0800

    完成扫码界面。

--- 新浪微博(OC版)/JDBarCodeController.m
+++ 新浪微博(OC版)/JDBarCodeController.m
@@ -11,6 +11,9 @@
 
 @interface JDBarCodeController ()
 
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *barCodeConstraintY;
+@property (nonatomic, strong) CADisplayLink *link;
+
 @end
 
 @implementation JDBarCodeController
@@ -27,6 +30,17 @@
     [self.navigationController.tabBarItem setSelectedImage:[UIImage imageNamed:@"qrcode_tabbar_icon_barcode"]];
     [self.navigationController.tabBarItem setSelectedImage:[[UIImage imageNamed:@"qrcode_tabbar_icon_barcode_highlighted"] getOriginalImage]];
     [self.navigationController.tabBarController.tabBar setTintColor:[UIColor orangeColor]];
+    
+    CADisplayLink *link = [CADisplayLink displayLinkWithTarget:self selector:@selector(scanning)];
+    [link addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
+    self.link = link;
+}
+
+-(void)scanning {
+    self.barCodeConstraintY.constant += 5;
+    if (self.barCodeConstraintY.constant >= 128) {
+        self.barCodeConstraintY.constant = -128;
+    }
 }
 
 @end
--- 新浪微博(OC版)/JDQRCodeController.m
+++ 新浪微博(OC版)/JDQRCodeController.m
@@ -11,6 +11,12 @@
 
 @interface JDQRCodeController ()
 
+/**
+ *  二维码扫描图片约束的Y值：
+ */
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *QRCodeConstraintY;
+@property (nonatomic, strong) CADisplayLink *link;
+
 @end
 
 @implementation JDQRCodeController
@@ -27,6 +33,20 @@
     [self.navigationController.tabBarItem setSelectedImage:[UIImage imageNamed:@"qrcode_tabbar_icon_qrcode"]];
     [self.navigationController.tabBarItem setSelectedImage:[[UIImage imageNamed:@"qrcode_tabbar_icon_qrcode_highlighted"] getOriginalImage]];
     [self.navigationController.tabBarController.tabBar setTintColor:[UIColor orangeColor]];
+    
+    CADisplayLink *link = [CADisplayLink displayLinkWithTarget:self selector:@selector(scanning)];
+    [link addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
+    self.link = link;
+}
+
+/**
+ *  扫描过程执行的方法：
+ */
+-(void)scanning {
+    self.QRCodeConstraintY.constant += 5;
+    if (self.QRCodeConstraintY.constant >= 170) {
+        self.QRCodeConstraintY.constant = -170;
+    }
 }
 
 @end
